10d8fe8133bd634fbd090ea5c74a8659c336b84d
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-08-18 10:43:37 -0700

    created thge category by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,9 +21,22 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoriesData = await Category.findByPk(req.params.id, {
+      include: [
+        {
+          model: Product,
+          attributes: ['product_name'],
+        },
+      ],
+    });
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
