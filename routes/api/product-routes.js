023663a1383277bb56fd2592c4b3f3a30451025a
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-08-18 12:37:17 -0700

    made get one products route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -30,9 +30,29 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        {
+          model: Category,
+        },
+        {
+          model: Tag,
+          attributes: ["id", "tag_name"],
+          through: {
+            model: ProductTag,
+            attributes: ["product_id", "tag_id"]
+          }
+        },
+      ],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
