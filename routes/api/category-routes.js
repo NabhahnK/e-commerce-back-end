7835c01a948616f6cea244c3be0aa53d0798241c
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-08-18 11:56:38 -0700

    made route to delete a category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -72,8 +72,27 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoriesData = await Category.destroy({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: Product,
+        },
+      ],
+    });
+
+    if (!categoriesData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
