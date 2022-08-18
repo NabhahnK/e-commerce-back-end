ea202cd84cbea7ed87560d05a7299b04f91ab6d5
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-08-18 13:06:58 -0700

    made get one tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -25,9 +25,26 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [
+        {
+          model: Product,
+          attributes: ["id", "product_name"],
+          through: {
+            model: ProductTag,
+            attributes: ["product_id", "tag_id"]
+          }
+        },
+      ],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
