c6aa39768ef9461bc2eeaf85b6b2271ef69d5f2e
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-08-18 13:33:02 -0700

    made update one tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -57,8 +57,27 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      include: [
+        {
+          model: Product,
+          attributes: ["id", "product_name"],
+          through: {
+            model: ProductTag,
+            attributes: ["product_id", "tag_id"]
+          }
+        },
+      ],
+      where: { id: req.params.id },
+    });
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
